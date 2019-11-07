be53bfda64bf5c69213e6ac3d4f7adca1e6393b3
Akseli Palén <akseli.palen@gmail.com>
2019-11-07 19:40:32 +0200

    puppeteer screenshot tryout

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer')
+
+const config = {
+  url: 'https://www.foreca.fi/Finland/Tammela/Kyynara',
+  elemId: 'mgwrap'
+};
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1024,
+    height: 1024
+  })
+  await page.goto(config.url);
+  setTimeout(async () => {
+    const r = await page.evaluate((queryPath) => {
+      var elem = document.querySelector(queryPath)
+      var rect = elem.getBoundingClientRect()
+      return {
+        x: rect.x,
+        y: rect.y,
+        width: rect.width,
+        height: rect.height
+      }
+    }, '#' + config.elemId)
+
+    await page.screenshot({
+      path: 'example.png',
+      clip: r
+    });
+    await browser.close();
+  }, 8000)
+})();
